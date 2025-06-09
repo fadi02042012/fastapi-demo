43b539f8ac098d1f2c4ad947cef6e49f9199bfd1
fadi02042012 <59389311+fadi02042012@users.noreply.github.com>
2025-06-09 11:59:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+﻿from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "أهلاً بك في API!"}
+
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"مرحبًا يا {name}!"}
